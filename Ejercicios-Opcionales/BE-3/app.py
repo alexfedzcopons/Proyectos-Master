2a7479ccdbf5bfaab10d2e520c84db49799552ab
Alejandro <alexfedzcopons@gmail.com>
2025-04-11 13:05:07 +0200

    Añadido BBDD SQLite y metodos PUT y DELETE

--- Ejercicios-Opcionales/BE-3/app.py
+++ Ejercicios-Opcionales/BE-3/app.py
@@ -1,43 +1,74 @@
 from flask import Flask,request,jsonify
+import sqlite3
 import json
 
 app = Flask(__name__)
 
 
-products = []
+DATABASE ="products.db"
+
+
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+          conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS products_table(
+                id INTEGER PRIMARY KEY,
+                nombre TEXT NOT NULL,
+                precio REAL NOT NULL
+                )
+                """
+          )
+init_db()
 
 
 @app.route("/api/products", methods=["GET"])
 def get_product_list():
-        with open("products.json" , "r") as file:
-              data = json.load(file)
-        return jsonify({"products":data})
+        with sqlite3.connect(DATABASE) as conn:
+          cursor = conn.execute("SELECT * FROM products_table")
+          productos =[
+               {"id": row[0], "nombre": row[1] , "precio": row[2]}
+               for row in cursor.fetchall()
+               ]
+        return jsonify(productos)
 
 
 @app.route("/api/products", methods=["POST"])
-def create_product_list():
+def create_product():
     data = request.get_json()
 
-    if not data or "name" not in data or "price" not in data:
-         return jsonify({"error":"Faltan Campos"})
- 
-    with open("products.json" , "r") as file:
-        existing_products = json.load(file)
+    with sqlite3.connect(DATABASE) as conn:
+          cursor = conn.execute(
+                "INSERT INTO products_table(nombre,precio) VALUES (?,?)",
+                (data["nombre"],data["precio"])
+          )
+          conn.commit()
 
-    new_product = {
-        "id": len(existing_products)+1,
-        "name": data["name"],
-        "price": data["price"]   
-    }
+    return jsonify({"id":cursor.lastrowid}),201
 
-    existing_products.append(new_product)
+@app.route("/api/products/<int:id>", methods =["PUT"])
+def update_product(id):
+    data= request.json
+    with sqlite3.connect(DATABASE) as conn:
+          conn.execute(
+                "UPDATE products_table SET nombre=?, precio=? WHERE id=?",
+                (data["nombre"], data["precio"], id)
+          )
+          conn.commit()
 
-    with open("products.json" , "w") as file:
-          json.dump(existing_products, file, indent= 4)
-
-    return jsonify({"message":"Producto creado con Éxito","product":new_product})
+    return jsonify({"message":"Product Updated"}),201
 
 
+@app.route("/api/products/<int:id>", methods =["DELETE"])
+def remove_product(id):
+    with sqlite3.connect(DATABASE) as conn:
+          conn.execute(
+                (f"DELETE FROM products_table WHERE id={id}")
+                
+          )
+          conn.commit()
+
+    return jsonify({"message":"Product Removed"}),201
 
 
 if __name__ == "__main__":
